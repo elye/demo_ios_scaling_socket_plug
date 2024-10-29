2d94960e74ef19494a20b6b1de2cf0f6ca8da4df
elisha.lye <elisha.lye@rea-group.com>
2024-10-30 10:38:27 +1100

    Initial Commit

---
+++ Socket Plug Exp/Socket_Plug_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Socket_Plug_ExpApp.swift
+//  Socket Plug Exp
+//
+//  Created by Elisha Lye on 30/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Socket_Plug_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
